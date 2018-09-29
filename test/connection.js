8efc0315c44ef8a981ca46694f92ac2a2b7fbe61
Rose Parker <parker.rose@ymail.com>
2018-09-29 02:06:23 +0000

    added extra code to connection.js to make sure the connection is made before any tests are run

--- test/connection.js
+++ test/connection.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-//connect to mongodb
+//Connect to the db before tests run
+before(function(done){
+  //connect to mongodb
 mongoose.connect('mongodb://localhost/testaroo', { useNewUrlParser: true });
 
 mongoose.connection.once('open', function(){
     console.log('Connection has been made, now make fireworks...'); //this fires if connection is made
-    
+    done();
 }).on('error', function(error){
     console.log('Connection error:', error);
+});  
 });
