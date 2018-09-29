f1865bf854ecbea9bc97378245c9011afc2745f1
Rose Parker <parker.rose@ymail.com>
2018-09-29 02:20:52 +0000

    updated connection.js file to connect to MLab database and show new saved collections

--- test/connection.js
+++ test/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //Connect to the db before tests run
 before(function(done){
   //connect to mongodb
-mongoose.connect('mongodb://localhost/testaroo', { useNewUrlParser: true });
+mongoose.connect('mongodb://titamandarica34:Freedom1!@ds117719.mlab.com:17719/testaroo', { useNewUrlParser: true });
 
 mongoose.connection.once('open', function(){
     console.log('Connection has been made, now make fireworks...'); //this fires if connection is made
