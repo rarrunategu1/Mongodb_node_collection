7d49e71967f2852caa25d9d71d6f4c4e57ef8bdb
Rose Parker <parker.rose@ymail.com>
2018-09-29 00:45:38 +0000

    Created Test folder with connection.js file for connectiong to database in Mongodb

---
+++ test/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost/testaroo');
+
+mongoose.connection.once('open', function(){
+    console.log('Connection has been made, now make fireworks...'); //this fires if connection is made
+    
+}).on('error', function(error){
+    console.log('Connection error:', error);
+});
